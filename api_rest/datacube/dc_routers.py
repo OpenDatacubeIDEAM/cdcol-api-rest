e7f762e894ff8d2efa82a81246e091fc5c170089
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-10-08 11:10:53 -0500

    Creados modelos y rutas para la conexión con la base de datos del Cubo.

---
+++ api_rest/datacube/dc_routers.py
@@ -0,0 +1,25 @@
+class DatacubeRouter(object):
+
+	def __init__(self):
+
+		self.dc_models = [
+		'Dataset',
+		'DatasetLocation',
+		'DatasetSource',
+		'DatasetType',
+		'MetadataType'
+		]
+
+	def db_for_read(self, model, **hints):
+
+		if model._meta.label.split('.',1)[-1] in self.dc_models:
+			return 'datacube'
+		return None
+
+	def db_for_write(self, model, **hints)
+
+		if model._meta.label.split('.',1)[-1] in self.dc_models:
+			# Temporarily deactivated
+			#	return 'datacube'
+			return None
+		return None

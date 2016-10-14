2ec445f3fc6c9323f9dc8fc11d81b6b0a396c431
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-10-14 11:17:13 -0500

    Se agrega la variable de entorno DC_STORAGE, corresponiente al 'root' del cubo de datos. Adicionalmente se crean los directorios correspondientes a la nueva unidad de almacenamiento.

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
 from api_rest.models import StorageUnit
 from StringIO import StringIO
-import base64, yaml
+import base64, yaml, os
 
 class StorageUnitSerializer(serializers.Serializer):
 
@@ -13,14 +13,20 @@ class StorageUnitSerializer(serializers.Serializer):
 	created_by = serializers.CharField(max_length=200)
 
 	def create(self, validated_data):
-		with open('/home/developer/Documentos/description_file.yaml', 'w') as desc_file:
+
+		stg_unit_folder = os.environ['DC_STORAGE'] + '/' + validated_data['name'].upper()
+		os.makedirs(stg_unit_folder)
+
+		with open(stg_unit_folder + '/description_file.yml', 'w') as desc_file:
 			desc_io = StringIO(validated_data['description_file'].replace('\\n','\n'))
 			base64.decode(desc_io, desc_file)
-		with open('/home/developer/Documentos/ingest_file.yaml', 'w') as desc_file:
+
+		with open(stg_unit_folder + '/ingest_file.yml', 'w') as desc_file:
 			desc_io = StringIO(validated_data['ingest_file'].replace('\\n','\n'))
 			base64.decode(desc_io, desc_file)
+
 		validated_data['metadata'] = ''
-		with open('/home/developer/Documentos/description_file.yaml', 'r') as metadata_file:
+		with open(stg_unit_folder + '/description_file.yml', 'r') as metadata_file:
 			metadata = yaml.load(metadata_file)
 			validated_data['metadata'] = JSONRenderer().render(metadata)
 		return object()

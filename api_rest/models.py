f6f47376513658150fbbe4172995b24bf41f3f92
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-10-14 13:20:08 -0500

    Agregado método print_all para revisar el contenido del modelo fácilmente

--- api_rest/models.py
+++ api_rest/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
+from rest_framework.renderers import JSONRenderer
 
 # Create your models here.
 class StorageUnit(models.Model):
@@ -17,4 +18,21 @@ class StorageUnit(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
-		return "{} - {}".format(self.id, self.name)
+		return "{}".format(self.name)
+
+	def print_all(self):
+		print 'Name: ' + self.name
+		print 'Description: ' + self.description
+		print 'Description file: ' + self.description_file
+		print 'Ingest file: ' + self.ingest_file
+		print 'Metadata: ' + JSONRenderer().render(self.metadata)
+		print 'Root directory: ' + self.root_dir
+		print 'Created by: ' + self.created_by.username
+		if self.created_at is None:
+			print 'Created at: None'
+		else:
+			print 'Created at: ' + str(self.created_at)
+		if self.updated_at is None:
+			print 'Updated at: None'
+		else:
+			print 'Updated at: ' + str(self.updated_at)

0fc5f736c14429e1dc148daafa528b436c9d817e
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-10-13 20:51:26 -0500

    Implementada vista de crear unidad de almacenamiento. El serializador correspondiente decodifica y almacena los yaml en disco (las rutas todavía están quemadas).

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from api_rest.models import StorageUnit
+from StringIO import StringIO
+import base64
 
-class StorageUnitSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = StorageUnit
+class StorageUnitSerializer(serializers.Serializer):
+
+	name = serializers.CharField(max_length=200)
+	description = serializers.CharField()
+	description_file = serializers.CharField()
+	ingest_file = serializers.CharField()
+	created_by = serializers.CharField(max_length=200)
+
+	def create(self, validated_data):
+		with open('/home/developer/Documentos/description_file.yaml', 'w') as desc_file:
+			desc_io = StringIO(validated_data['description_file'].replace('\\n','\n'))
+			base64.decode(desc_io, desc_file)
+		with open('/home/developer/Documentos/ingest_file.yaml', 'w') as desc_file:
+			desc_io = StringIO(validated_data['ingest_file'].replace('\\n','\n'))
+			base64.decode(desc_io, desc_file)
+		return object()
+		
--- api_rest/views.py
+++ api_rest/views.py
@@ -3,6 +3,8 @@ from rest_framework import response, schemas, viewsets
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 from api_rest.models import StorageUnit
 from api_rest.serializers import StorageUnitSerializer
+from rest_framework.parsers import JSONParser
+from StringIO import StringIO
 
 # View for swagger documentation
 @api_view()
@@ -15,4 +17,14 @@ def schema_view(request):
 class StorageUnitViewSet(viewsets.ModelViewSet):
 	queryset = StorageUnit.objects.all()
 	serializer_class = StorageUnitSerializer
-
+	
+	def perform_create(self, serializer):
+		if type(self.request.data) is dict:
+			json_content = self.request.data
+		elif type(self.request.data) is str:
+			json_str = self.request.data
+			json_io = StringIO(json_str)
+			json_content = JSONParser().parse(json_io)
+		serializer = StorageUnitSerializer(data=json_content)
+		if serializer.is_valid():
+			storage_unit = serializer.save()

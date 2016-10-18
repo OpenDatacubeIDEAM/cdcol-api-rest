61ed97c5ea02f529cad4fe8ceec9257378fee438
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-10-18 12:51:23 -0500

    Agregado servicio para eliminar unidad de almacenamiento. (Pendiente en base de datos del cubo)

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,10 +1,11 @@
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework import response, schemas, viewsets
+from rest_framework import response, schemas, viewsets, status
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 from api_rest.models import StorageUnit
 from api_rest.serializers import StorageUnitSerializer
 from rest_framework.parsers import JSONParser
 from StringIO import StringIO
+import shutil
 
 # View for swagger documentation
 @api_view()
@@ -28,3 +29,11 @@ class StorageUnitViewSet(viewsets.ModelViewSet):
 		serializer = StorageUnitSerializer(data=json_content)
 		if serializer.is_valid():
 			storage_unit = serializer.save()
+			serializer = StorageUnitSerializer(storage_unit)
+			return response.Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+	def perform_destroy(self, instance):
+		root_dir = instance.root_dir
+		instance.delete()
+		shutil.rmtree(root_dir)
+		return response.Response(data={'status' : 'Storage Unit Deleted' }, status=status.HTTP_204_NO_CONTENT)

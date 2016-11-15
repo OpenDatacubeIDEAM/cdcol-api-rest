2c54bf5d716c220248c00605ca549c9ab8f37971
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-11-15 15:14:34 -0500

    Actualizada zona horaria en archivo settings.py

--- cdcol/settings.py
+++ cdcol/settings.py
@@ -120,16 +120,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/

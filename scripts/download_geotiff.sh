e42b825603ef191f448f365390193a4ade41bfbe
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2017-01-17 18:21:11 -0500

    Agregado nuevo script para generar archivos geotiff a partir de netcdf

---
+++ scripts/download_geotiff.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#Script para la generación de thumbnails de resultados, 
+#debería ser llamado por el cron que revisa el estado de la ejecución, 
+#cuando encuentra que una ejecución ha terminado correctamente.
+#Parámetros Opcionales:Carpeta resolución
+
+#GDAL_DATA Debería estar definida en el entorno, si no, toca definirla.
+export GDAL_DATA="${GDAL_DATA:-/usr/share/gdal/1.11}"
+
+FILE="$1"
+BN=`basename $FILE`
+FOLDER="${1%/$BN}"
+
+if `gdalinfo $FILE |grep -q "SUBDATASET.*"`
+then
+	for ds in `gdalinfo $FILE | grep -E "NETCDF.*"`
+	do
+	band_number=$(echo $ds | sed -e 's/^[^0-9]*\([0-9]*\).*/\1/')
+	echo "Escribiendo el archivo $FILE y la banda ${ds##*\:} ($band_number)"
+	# gdal_translate  -a_srs EPSG:32618 -stats -of PNG -scale -ot Byte -outsize $RES $RES $ds $TN_FOLDER/${bn%.nc}.${ds##*\:}.png
+	gdal_translate -a_srs EPSG:32618 -stats ${ds#*\=} $FOLDER/${BN%.nc}.${ds##*\:}.$band_number.tiff
+	done
+else
+	
+	nb=`gdalinfo $FILE |grep  Band|wc -l`
+	echo $nb 
+	if [[ $nb -le 1 ]]
+	then
+		echo "Escribiendo el thumbnail para el archivo $FILE"
+		# gdal_translate  -a_srs EPSG:32618  -stats -of PNG -scale -ot Byte -outsize $RES $RES $FILE $TN_FOLDER/${bn%.nc}.png
+		gdal_translate -a_srs EPSG:32618 -stats $FILE $FOLDER/${BN%.nc}.tiff
+	else
+		for i in $(seq 1 $nb)
+		do
+		echo "Escribiendo el thumbnail para el archivo $FILE banda $i"
+
+		gdal_translate -a_srs EPSG:32618 -stats $FILE $FOLDER/${BN%.nc}.$i.tiff
+		done
+	fi
+fi
+
+gdal_merge.py -separate -o ${FILE%.*}.tiff $(ls ${FILE%.*}.*.tiff | sed -e 's/\(^.*\.\([0-9]*\)\.tiff\)/\2_\1/' | sort -t _ -k 1 -n | sed -e 's/^[0-9]*_\(.*\)/\1/')
+rm ${FILE%.*}.*.tiff

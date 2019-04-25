389992280aa79f1c393f606296f2a537f39f964c
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-04-25 07:12:55 -0500

    es version_id no algorithm_id

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -292,14 +292,15 @@ class AlgorithmSerializer(serializers.Serializer):
     def create(self, validated_data):
         extraction_path = os.path.join(os.environ['DOWNLOAD_PATH'], str(validated_data["version_id"]))
         version = Version.objects.filter(id=validated_data["version_id"])
-		if not version.exists():
-			raise serializers.ValidationError('No existe una versión con el id: '+str(validated_data["version_id"]))
 
         with zipfile.ZipFile(validated_data["algorithms_zip_file"], "r") as file_to_extract:
             file_to_extract.extractall(extraction_path)
 
         # TODO: Poner el template en la carpeta templates
+        if not version.exists():
+            raise serializers.ValidationError('No existe una version con el id {} '.format(validated_data["version_id"]))
         template_path = os.path.join(os.environ['TEMPLATE_PATH'], version.algorithm.name)
+
         if not os.path.isdir(template_path):
             os.makedirs(template_path)
 
